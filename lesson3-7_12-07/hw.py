19db2d09220371c43aa685bf8b5c1bffa8e11555
Liza-c <lizakharchenko6@gmail.com>
2021-07-13 17:50:40 +0200

    hw

--- lesson3-7_12-07/hw.py
+++ lesson3-7_12-07/hw.py
@@ -1,1 +1,20 @@
-print("hello")
+class Car:
+    def __init__(self, model, year, engine, color, price):
+        self.model = model
+        self.year = year
+        self.engine = engine
+        self.color = color
+        self.price = price
+    
+    def car_parameters(self):
+        return f"This is {self.model}, {self.year} release, engine size: {self.engine}, "\
+            f"car color: {self.color}, price: {self.price}:"
+    
+camry = Car(
+    model= 'Camry', 
+    year= 2015, 
+    engine= '3.5', 
+    color = 'red', 
+    price = 14000
+    )
+        
